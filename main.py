ceac682843e3eb060c396594aa1cfacdea5ba92f
VarunMarda777 <138223303+VarunMarda777@users.noreply.github.com>
2025-12-18 19:32:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-print("Project is running on GitHub Actions")
+print("Website Blocker Simulation – GitHub Run")
 
 sites = ["facebook.com", "youtube.com", "gmail.com"]
 
-for site in sites:
-    print(f"Blocked: {site}")
+with open("data/hosts_simulated.txt", "w") as f:
+    for site in sites:
+        f.write(f"127.0.0.1 {site}\n")
+        print(f"Blocked: {site}")
+
+print("Simulation completed")
